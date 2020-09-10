ba91b38a88a7c06a8e8227b22442438e3683946d
Xian Mo <nextstep.mo@gmail.com>
2020-09-10 15:57:39 +0800

    init project

---
+++ MVBSwift/MVBSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVBSwift
+//
+//  Created by Xian Mo on 2020/9/10.
+//  Copyright © 2020 Mo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
